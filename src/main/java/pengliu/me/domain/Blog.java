50b7e80bc503010b43cb199a3038910bcca12ca9
liupeng330 <330liupeng@gmail.com>
2016-04-08 19:11:16 +0800

    create database domain, with hibernate mapping files

---
+++ src/main/java/pengliu/me/domain/Blog.java
@@ -0,0 +1,206 @@
+package pengliu.me.domain;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by peng on 16-4-8.
+ */
+@Entity
+public class Blog
+{
+    private Integer id;
+    private String title;
+    private String summary;
+    private String content;
+    private Timestamp updateTime;
+    private Timestamp createTime;
+    private String status;
+    private Integer showCount;
+    private User user;
+    private Category category;
+    private Set<Tag> tags = new HashSet<Tag>();
+    private Set<Comment> comments = new HashSet<Comment>();
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = false)
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "summary", nullable = false)
+    public String getSummary()
+    {
+        return summary;
+    }
+
+    public void setSummary(String summary)
+    {
+        this.summary = summary;
+    }
+
+    @Basic
+    @Column(name = "content", nullable = false)
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+
+    @Basic
+    @Column(name = "update_time", nullable = false)
+    public Timestamp getUpdateTime()
+    {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime)
+    {
+        this.updateTime = updateTime;
+    }
+
+    @Basic
+    @Column(name = "create_time", nullable = false)
+    public Timestamp getCreateTime()
+    {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime)
+    {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = false)
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "show_count", nullable = false)
+    public Integer getShowCount()
+    {
+        return showCount;
+    }
+
+    public void setShowCount(Integer showCount)
+    {
+        this.showCount = showCount;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Blog blog = (Blog) o;
+
+        if (id != null ? !id.equals(blog.id) : blog.id != null) return false;
+        if (title != null ? !title.equals(blog.title) : blog.title != null) return false;
+        if (summary != null ? !summary.equals(blog.summary) : blog.summary != null) return false;
+        if (content != null ? !content.equals(blog.content) : blog.content != null) return false;
+        if (updateTime != null ? !updateTime.equals(blog.updateTime) : blog.updateTime != null) return false;
+        if (createTime != null ? !createTime.equals(blog.createTime) : blog.createTime != null) return false;
+        if (status != null ? !status.equals(blog.status) : blog.status != null) return false;
+        if (showCount != null ? !showCount.equals(blog.showCount) : blog.showCount != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (summary != null ? summary.hashCode() : 0);
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (updateTime != null ? updateTime.hashCode() : 0);
+        result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (showCount != null ? showCount.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "blog_tag",
+            joinColumns = {@JoinColumn(name = "blog_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "tag_id", nullable = false, updatable = false)})
+    public Set<Tag> getTags()
+    {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags)
+    {
+        this.tags = tags;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    public User getUser()
+    {
+        return user;
+    }
+
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id", nullable = false)
+    public Category getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(Category category)
+    {
+        this.category = category;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "blog")
+    public Set<Comment> getComments()
+    {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments)
+    {
+        this.comments = comments;
+    }
+}
---
+++ src/main/java/pengliu/me/domain/User.java
@@ -0,0 +1,121 @@
+package pengliu.me.domain;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by peng on 16-4-8.
+ */
+@Entity
+public class User
+{
+    private Integer id;
+    private String name;
+    private String password;
+    private String status;
+    private Timestamp lastLoginTime;
+    private Set<Blog> blogs = new HashSet<Blog>();
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false)
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false)
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = false)
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "last_login_time", nullable = false)
+    public Timestamp getLastLoginTime()
+    {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Timestamp lastLoginTime)
+    {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (status != null ? !status.equals(user.status) : user.status != null) return false;
+        if (lastLoginTime != null ? !lastLoginTime.equals(user.lastLoginTime) : user.lastLoginTime != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (lastLoginTime != null ? lastLoginTime.hashCode() : 0);
+        return result;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    public Set<Blog> getBlogs()
+    {
+        return blogs;
+    }
+
+    public void setBlogs(Set<Blog> blogs)
+    {
+        this.blogs = blogs;
+    }
+}
