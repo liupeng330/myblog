c4d393feb680759435cc9bf4c041e6790d344a4d
liupeng330 <330liupeng@gmail.com>
2016-06-07 16:28:25 +0800

    adding build sh

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+mvn_bin_path=/home/peng/apache-maven-3.3.3/bin/mvn
+war_file_name=myblog-1.0-SNAPSHOT.war
+tomcat_home_path=/home/peng/apache-tomcat-8.0.24
+tomcat_bin_path=$tomcat_home_path/bin/catalina.sh
+webapp_name=myblog
+
+echo "Deleting 'out' and 'target' folder."
+rm -rf out target
+
+echo "Running 'mvn clean package' to build codes."
+$mvn_bin_path clean package -Dmaven.test.skip=true
+
+echo "Checking if 'target/$war_file_name' exist or not ??"
+if [ ! -f "target/$war_file_name" ]; then
+    echo "The build war file 'target/$war_file_name' doesn't exist !! "
+    exit 1
+fi
+echo "The build war file exists."
+
+echo "Starting to stop tomcat server."
+$tomcat_bin_path stop -force
+
+if [ `ps aux|grep java|grep $tomcat_home_path|awk '{print $2}'` ];then
+    echo "Killing tomcat process."
+    ps aux|grep java|grep $tomcat_home_path|awk '{print $2}'|xargs -I {} kill -9 {}
+fi
+
+echo "Removing war and folder under tomcat home folder."
+rm -rf $tomcat_home_path/webapps/$webapp_name.war
+rm -rf $tomcat_home_path/webapps/$webapp_name
+
+echo "Copying build war file to tomcat webapp folder."
+cp target/$war_file_name $tomcat_home_path/webapps/$webapp_name.war
+
+echo "Starting to startup tomcat service."
+$tomcat_bin_path start
+
+echo "Done for deploy!!"
